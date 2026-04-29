5bcf9b4b2f0f02305374ef4274582d76b7e38bdc
Lorenzo <neuronefunzionante@gmail.com>
2026-04-29 09:01:31 +0100

    clean up legacy stuff

--- apps/lending-api/src/app.ts
+++
@@ -1,9 +0,0 @@
-import Fastify, { FastifyInstance } from 'fastify'
-
-export function buildApp(): FastifyInstance {
-  const fastify = Fastify({ logger: true })
-
-  fastify.get('/', async () => ({ status: 'ok' }))
-
-  return fastify
-}
